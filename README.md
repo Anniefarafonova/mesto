abe97fe2628a4b9795e1e5d396bb7a2003d00086
Anna Farafonova <annie.hop@yandex.ru>
2023-03-11 18:47:36 +0300

    commit

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Проект: Место
 
-### Обзор
+## Цели проекта
+- Сделать сайт адаптивным;
+- Реализовать: попап редактирования профиля / открытие и закрытие попапа
 
-* Figma
-* Картинки
+[**Ссылка на макет Figma**](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
-**Figma**
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+[**Ссылка на макет Figma**](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
-**Картинки**
 
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+[**Ссылка на сайт**]()
---
+++ index.js
@@ -0,0 +1,51 @@
+const popupElement = document.querySelector(".popup");
+const popupCloseButtonElement = popupElement.querySelector(".popup__close");
+const popupCavedButtonElement = popupElement.querySelector(".popup__saved-button");
+const profileButtonElement = document.querySelector(".profile");
+const profileEditButtonElement = profileButtonElement.querySelector(".profile__edit-button");
+const profileAddButtonElement = profileButtonElement.querySelector(".profile__add-button");
+
+const EditButton = function () {
+    popupElement.classList.add("popup_opened")
+    console.log("Edit Button");
+};
+const CloseButton = function () {
+    popupElement.classList.remove("popup_opened")
+    console.log("Close Button");
+};
+
+
+const CloseButtonByClickOnOverlay = function (event) {
+    console.log(event.target, event.currentTarget);
+    if (event.target === event.currentTarget) {
+        CloseButton()
+    }
+}
+
+popupCloseButtonElement.addEventListener("click", CloseButton);
+profileEditButtonElement.addEventListener("click", EditButton);
+popupElement.addEventListener("click", CloseButtonByClickOnOverlay)
+
+
+popupCavedButtonElement
+// Находим форму в DOM
+let formElement = document.querySelector(".form__list");
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".form__item_name");// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".form__item_theme");// Воспользуйтесь инструментом .querySelector()
+let nameTitle = profileButtonElement.querySelector(".profile__title");
+let jobSubtitle = profileButtonElement.querySelector(".profile__subtitle");
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
+nameTitle.textContent = nameInput.value;
+jobSubtitle.textContent = jobInput.value;
+CloseButton()
+
+}
+formElement.addEventListener('submit', handleFormSubmit); 
+
+
+
