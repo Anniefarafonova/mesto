64ae23a05ab7c04a3e22f71496fc9107bc67f8af
Anna Farafonova <annie.hop@yandex.ru>
2023-03-13 00:25:29 +0300

    test2

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,44 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupCavedButtonElement = popupElement.querySelector(".popup__saved-button");
 const profileButtonElement = document.querySelector(".profile");
 const profileEditButtonElement = profileButtonElement.querySelector(".profile__edit-button");
-const profileAddButtonElement = profileButtonElement.querySelector(".profile__add-button");
 
-const EditButton = function () {
+const editButton = function () {
     popupElement.classList.add("popup_opened")
-    console.log("Edit Button");
+    nameTitle.value = nameInput.textContent;
+    jobSubtitle.value = jobInput.textContent;
 };
-const CloseButton = function () {
+const closeButton = function () {
     popupElement.classList.remove("popup_opened")
-    console.log("Close Button");
 };
 
-
-const CloseButtonByClickOnOverlay = function (event) {
+/*const closeButtonByClickOnOverlay = function (event) {
     console.log(event.target, event.currentTarget);
     if (event.target === event.currentTarget) {
-        CloseButton()
+        closeButton()
     }
-}
+}  
+popupElement.addEventListener("click", closeButtonByClickOnOverlay); */
 
-popupCloseButtonElement.addEventListener("click", CloseButton);
-profileEditButtonElement.addEventListener("click", EditButton);
-popupElement.addEventListener("click", CloseButtonByClickOnOverlay)
+popupCloseButtonElement.addEventListener("click", closeButton);
+profileEditButtonElement.addEventListener("click", editButton);
 
 
-popupCavedButtonElement
+
 // Находим форму в DOM
 let formElement = document.querySelector(".form");
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector(".form__item_name");// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".form__item_theme");// Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector(".form__item_type_name");// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".form__item_type_job");// Воспользуйтесь инструментом .querySelector()
 let nameTitle = profileButtonElement.querySelector(".profile__title");
 let jobSubtitle = profileButtonElement.querySelector(".profile__subtitle");
 
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-
 nameTitle.textContent = nameInput.value;
 jobSubtitle.textContent = jobInput.value;
-CloseButton()
-
+closeButton()
 }
 formElement.addEventListener('submit', handleFormSubmit); 
 
