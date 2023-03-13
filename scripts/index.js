a6a9b549ef1fe3aa09f14ea4de161d6280295189
Anna Farafonova <annie.hop@yandex.ru>
2023-03-13 22:11:34 +0300

    test3

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,14 @@ const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const profileButtonElement = document.querySelector(".profile");
 const profileEditButtonElement = profileButtonElement.querySelector(".profile__edit-button");
 
+let formElement = document.querySelector(".form");
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".form__item_type_name");// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".form__item_type_job");// Воспользуйтесь инструментом .querySelector()
+let nameTitle = profileButtonElement.querySelector(".profile__title");
+let jobSubtitle = profileButtonElement.querySelector(".profile__subtitle");
+
+
 const editButton = function () {
     popupElement.classList.add("popup_opened")
     nameTitle.value = nameInput.textContent;
@@ -23,15 +31,6 @@ popupElement.addEventListener("click", closeButtonByClickOnOverlay); */
 popupCloseButtonElement.addEventListener("click", closeButton);
 profileEditButtonElement.addEventListener("click", editButton);
 
-
-
-// Находим форму в DOM
-let formElement = document.querySelector(".form");
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector(".form__item_type_name");// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".form__item_type_job");// Воспользуйтесь инструментом .querySelector()
-let nameTitle = profileButtonElement.querySelector(".profile__title");
-let jobSubtitle = profileButtonElement.querySelector(".profile__subtitle");
 
 
 function handleFormSubmit (evt) {
